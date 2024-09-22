c5b308c697fd27c2b3854d677059d0a4a735682c
Arkadiusz Zając <arekzaj7@gmail.com>
2024-09-22 23:12:54 +0200

    Response converter test 4

--- engineers-thesis/src/utils/jsonResponseConverter.ts
+++ engineers-thesis/src/utils/jsonResponseConverter.ts
@@ -3,10 +3,8 @@ import { weatherCondition } from "../types/weather-condition";
 
 export async function jsonResponseConverter(response: Response): Promise<weatherCondition[]> {
     console.log(response);
-    const responseJson = await response.json() as string;
+    const responseJson = await response.json() as responseJson;
     console.log(responseJson);
-    const parsedObject: responseJson = JSON.parse(responseJson);
-    console.log(parsedObject);
 
-    return parsedObject.days;
+    return responseJson.days;
 }
