9cd104e645ce5551abc99088429f2831f76cf29c
Arkadiusz Zając <arekzaj7@gmail.com>
2024-10-06 17:14:10 +0200

    Test

--- engineers-thesis/src/features/raport-by-location/raport-by-location.tsx
+++ engineers-thesis/src/features/raport-by-location/raport-by-location.tsx
@@ -14,29 +14,31 @@ const RaportByLocation = () => {
     const [isSet, setIsSet] = useState<boolean>(false);
 
     useEffect(() => {
+        const weatherConditions: weatherCondition[] = [];
+
         const getWeatherConditions = async () => {
             const datePagination = createPagination();
-
-            const weatherConditions: weatherCondition[] = [];
+            console.log("Pagination: " + datePagination);
+            
             datePagination.forEach(async (dateRange) => {
                 const responseJson = await fetch(`/weather/${lat}/${lng}/${dateRange.start}/${dateRange.end}`);
                 const response = await jsonResponseConverter(responseJson);
                 weatherConditions.push(...response);
-                console.log(response);
-                console.log(weatherConditions);
+                console.log("Response: " + response);
+                console.log("Weather Conditions: " + weatherConditions);
             });
-
-            if(weatherConditions.length >= 50) {
-                setWeatherData({
-                    latitude: parseFloat(lat as string),
-                    longitude: parseFloat(lng as string),
-                    weatherConditions: weatherConditions as weatherCondition[]
-                });
-                setIsSet(true);
-            }
         }
     
         getWeatherConditions();
+        console.log("Weather Conditions End: " + weatherConditions);
+        setWeatherData({
+            latitude: parseFloat(lat as string),
+            longitude: parseFloat(lng as string),
+            weatherConditions: weatherConditions as weatherCondition[]
+        });
+        console.log("Weather Data: " + weatherData);
+        setIsSet(true);
+        console.log("Set: " + isSet);
     }, [lat, lng, isSet, weatherData]);
 
     return (
